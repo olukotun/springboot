0a8a82d02e02460554a866048a031a702b878d7a
Oludamilare Olukotun <olukotuno@gmail.com>
2020-10-17 02:37:19 -0400

    Code not working cos dao not autowired

--- src/main/java/com/olukotun/springdataenversauditing/BookService/BookServices.java
+++ src/main/java/com/olukotun/springdataenversauditing/BookService/BookServices.java
@@ -4,6 +4,7 @@ import com.olukotun.springdataenversauditing.BookDA0.BookDAO;
 import com.olukotun.springdataenversauditing.BookDTO.Book;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -13,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 @Service
 public class BookServices {
+    @Autowired
     private BookDAO bookDAO;
 
     public Book saveBook(Book book) {
--- src/main/java/com/olukotun/springdataenversauditing/SpringDataEnversAuditingApplication.java
+++ src/main/java/com/olukotun/springdataenversauditing/SpringDataEnversAuditingApplication.java
@@ -1,7 +1,6 @@
 package com.olukotun.springdataenversauditing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
